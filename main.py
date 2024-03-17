162b9b70c7c011e5f0014443dfd6b8256483f51a
Zgn75 <70374584+Zgn75@users.noreply.github.com>
2024-03-18 00:31:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ bot = commands.Bot(command_prefix='?', intents=intents, help_command=None)
 
 #Local variables
 loop = False
+token = "bot-token"
 owner_id = int("your-user-id")
 colors = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xe67e22, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a, 0x7289da]
 
@@ -319,4 +320,4 @@ async def quit(ctx):
     await ctx.message.delete()
     await bot.close()
 
-bot.run("token")  # Where 'TOKEN' is your bot token
+bot.run(token)  # Where 'TOKEN' is your bot token
